45bfd37d015023e9dee919cb9f6b7b82cd4ecd18
rakaxlfux <159986888+rakaxlfux@users.noreply.github.com>
2024-03-26 14:26:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ client.once(Events.ClientReady, readyClient => {
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
+
 	const command = interaction.client.commands.get(interaction.commandName);
-    }
 
 	if (!command) {
 		console.error(`No command matching ${interaction.commandName} was found.`);
